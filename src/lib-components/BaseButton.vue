9fe8b564a5f6b6890a9c4bb5a765e30d68b0fde0
Felipe Barreto <felipebarreto148@gmail.com>
2022-07-13 17:29:36 -0300

    feat: Init project

---
+++ src/lib-components/BaseButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <button :style="styleSheet" @click="$emit('clicked')">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { BaseButtonProps } from "@/types/base-button"
+
+export default Vue.extend({
+  props: {
+    background: {
+      type: String,
+      default: () => "orange"
+    },
+    color: {
+      type: String,
+      default: () => "black"
+    },
+  },
+  computed: {
+    styleSheet(): BaseButtonProps{
+      return {
+        background: this.background,
+        color: this.color,
+      }
+    }
+  }
+})
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&display=swap');
+button {
+  width: 200px;
+  height: 50px;
+  border: none;
+  border-radius: 8px;
+  transition: ease-in 200ms;
+  font: 500 18px 'Fira Code', monospace;
+  cursor: pointer;
+}
+
+button:hover {
+  transform: scale3d(1.1, 1.1, 1.1);
+}
+</style>
