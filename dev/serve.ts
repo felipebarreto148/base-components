55170ac26c7a1b0480c3ce98cae16d6f7b5b3c7f
Felipe Barreto <felipebarreto148@gmail.com>
2022-07-14 21:19:47 -0300

    use mdi font

--- dev/serve.ts
+++ dev/serve.ts
@@ -1,9 +1,15 @@
 import Vue, { VNode } from 'vue';
 import Dev from './serve.vue';
+import mdiVue from "mdi-vue/v2";
+import * as mdiJs from "@mdi/js";
 // To register individual components where they are used (serve.vue) instead of using the
 // library as a whole, comment/remove this import and it's corresponding "Vue.use" call
 import MyBaseComponents from '@/entry.esm';
 Vue.use(MyBaseComponents);
+Vue.use(mdiVue, {
+  icons: mdiJs
+})
+
 
 Vue.config.productionTip = false;
 
--- dev/serve.vue
+++ dev/serve.vue
@@ -1,13 +1,41 @@
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'ServeDev',
+  name: "ServeDev",
 });
 </script>
 
 <template>
   <div id="app">
-    <base-button>Bem vindo</base-button>
+    <main class="content">
+      <base-button>
+        Bem vindo
+        <template v-slot:prepend>
+          <mdicon name="chevron-right" />
+        </template>
+      </base-button>
+    </main>
   </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html,
+body,
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+
+.content {
+  width: 100%;
+  height: 100%;
+  background: lightblue;
+  padding: 20px;
+}
+</style>
